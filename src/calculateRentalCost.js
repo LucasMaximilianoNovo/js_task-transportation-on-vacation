e0ca3cd3f15273a14f3a4c23b19ceb3556302c31
Lucas Maximiliano Novo <lucasmaxnovo123@gmail.com>
2025-12-12 19:43:04 -0300

    add tesk solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = 40;
+  const totalRent = rentalCost * days;
+  const discounted20RentalCost = totalRent - 20;
+  const discounted50RentalCost = totalRent - 50;
+
+  if (days < 3) {
+    return totalRent;
+  }
+
+  if (days < 7) {
+    return discounted20RentalCost;
+  }
+
+  return discounted50RentalCost;
 }
 
 module.exports = calculateRentalCost;
